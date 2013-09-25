e7642e24d055abf5a336cb7a5c968e02e7b87203
Federico Tomassetti <f.tomassetti@gmail.com>
2013-09-25 10:12:29 +0200

    Adding some tests

---
+++ test/test_parsing_puzzle.rb
@@ -0,0 +1,38 @@
+require 'test/unit'
+require 'lightmodels'
+require 'html-lightmodels'
+require 'test_helper'
+ 
+class TestParsingPuzzle < Test::Unit::TestCase
+
+	include TestHelper
+	include LightModels
+	include LightModels::Html
+
+	def test_root_structure
+		code = IO.read('test/data/puzzle.html')
+		r = Html.parse_code(code)
+		assert_class HtmlDocument, r
+
+		# child 0: <!DOCTYPE html>
+		assert_equal 2, r.children.count
+		assert_class Node, r.children[1] 	
+		assert_equal 'html', r.children[1].name
+	end
+
+	def test_body_tag
+		code = IO.read('test/data/puzzle.html')
+		r = Html.parse_code(code)
+
+		body = nil
+		r.traverse do |n|
+			body=n if n.is_a?(Node) && n.name=='body'
+		end
+		assert_not_nil body
+		assert_equal 1,body.attributes.count
+		assert_equal 'ng-app',body.attributes[0].name
+		assert_equal 'puzzleApp',body.attributes[0].value
+	end
+	
+
+end
